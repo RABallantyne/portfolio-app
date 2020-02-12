993a3739390ba3e15a59cfb49de14628fd9fb851
Jerkbert <raballantyne@gmail.com>
2020-02-12 16:21:44 -0700

    added emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
+  const sendEmail = e => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        `gmail`,
+        `contact_portfolio`,
+        e.target,
+        `user_4DgMKGqJscoo7Q4S1HIwG`
+      )
+      .then(
+        result => {
+          console.log(result.text);
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div>
-      <h1>email me here</h1>
-    </div>
+    <form className='contact-form' onSubmit={sendEmail}>
+      <input type='hidden' name='contact_number' />
+      <label>Name</label>
+      <input type='text' name='user_name' />
+      <label>Email</label>
+      <input type='email' name='user_email' />
+      <label>Message</label>
+      <textarea name='message' />
+      <input type='submit' value='Send' />
+    </form>
   );
 }
